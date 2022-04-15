aac6360d5e7d993df21498fbec84c98ab778a279
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-04-15 12:42:21 +0530

    Add files via upload

---
+++ Basic programs/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{ 
+  int No1=0,No2=0,sum=0;
+  
+  printf("\n Enter two no=");
+  scanf("%d%d",&No1,&No2);
+  
+     sum=No1+No2;
+     
+   printf("\n\a Addition of two numbers=%d",sum);
+   
+   return 0;
+}

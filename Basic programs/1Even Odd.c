51a8f7834cdc85dad5fc3e204ba451bcb7ab1b7e
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-04-18 18:41:16 +0530

    Add files via upload

---
+++ Basic programs/1Even Odd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num=0;
+    
+    printf("\n Enter an Integer Number = ");
+    scanf("%d",&Num);
+    
+    if(Num % 2 == 0)
+    {
+          printf("\n Given Number is EVEN.");
+    }
+    else
+    {
+          printf("\n Given Number is ODD.");
+    }
+    
+    getch();
+    return 0;
+    
+}    

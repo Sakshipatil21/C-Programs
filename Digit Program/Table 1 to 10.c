8ed8d897a52a43101e0351c25d2d7f8c9cc12d2e
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-18 20:33:08 +0530

    Add files via upload

---
+++ Digit Program/Table 1 to 10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i=0,j=0;
+  printf("Multiplication Table= \n\n");
+  
+  for(i=1;i<=10;i++)
+  {
+    for(j=1;j<=10;j++)
+    {
+      printf("%4d",i*j);
+    }
+    printf(" \n");
+  }
+  return 0;
+  
+}

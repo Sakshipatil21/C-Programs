806a1e640aabcf1b082e9802f7d2a7f2042c5a83
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-22 19:28:19 +0530

    Update Program To a Accept Ten Numbers And Display Their Summation.c

--- Digit Program/Program To a Accept Ten Numbers And Display Their Summation.c
+++ Digit Program/Program To a Accept Ten Numbers And Display Their Summation.c
@@ -4,7 +4,7 @@ int main()
 {
   int No=0,Sum=0,Cnt=0;
   
-  printf("\n Enter 10 Numbes=>");
+  printf("\n Enter 10 Numbers=>");
   
   for(Cnt=1;Cnt<=10;Cnt++)
   {
@@ -13,10 +13,10 @@ int main()
     
     Sum = Sum + No;
   }
-  printf("\n Press any key to get summation of given numbers=>");
+  printf("\n Press Any Key To Get Summation Of Given Numbers=>");
   
   getch();
   clrscr();
   
-  printf("\n Summation of given numbes= %d",Sum);
+  printf("\n Summation of given numbers= %d",Sum);
 }

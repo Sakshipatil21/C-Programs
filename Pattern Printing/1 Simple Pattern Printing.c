00ee05d5fe9d90709a3510145eed994de7199b6f
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-23 20:19:12 +0530

    Add files via upload

---
+++ Pattern Printing/1 Simple Pattern Printing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int r = 0,c = 0;
+   
+   printf("\n Pattern is => \n\n");
+   
+   for(r=1;r<=4;r++)
+   {
+      for(c=1;c<=5;c++)
+      {
+        printf(" * ");
+      }
+      printf("\n");
+   }
+   return 0;
+}

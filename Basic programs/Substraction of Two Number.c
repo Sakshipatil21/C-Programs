59b0ef0279f698367c6ac455823938119de80f8c
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-24 20:17:53 +0530

    Add files via upload

---
+++ Basic programs/Substraction of Two Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int No1 = 0,No2 = 0,Sub = 0;
+   
+   printf("\n Enter First Number =");
+   scanf("%d",&No1);
+   printf("\n Enter Second Number =");
+   scanf("%d",&No2);
+  
+   Sub = No1 - No2;
+   
+   printf("\n Substraction of Two Numbers = %d",Sub);
+   
+   getch();
+   return 0;
+ }

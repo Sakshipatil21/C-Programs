6a3a4301c3c403952228a00a2adbb12f68aa5f4b
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-24 10:31:27 +0530

    Add files via upload

---
+++ Pattern Printing/Right Lower Triangle Pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int r = 0,c = 0,cnt = 0;
+  
+  printf("\n Enter Count of Rows and Columns =");
+  scanf("%d",&cnt);
+  
+  printf("\n\n Pattern is.....\n\n");
+  
+  for(r=1;r<=cnt;r++)
+  {
+    for(c=1;c<=cnt;c++)
+    {
+      if(r+c>=cnt+1)
+      {
+        printf(" * ");
+      }
+      else
+      {
+        printf("   ");
+      }
+    }
+    printf("\n");
+  }
+  getch();
+  return 0;
+}

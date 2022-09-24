c0ddf4394fc6f54e1db5e28a38be53949c9b1a22
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-24 20:25:16 +0530

    Add files via upload

---
+++ Basic programs/Division of Two Numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int No1 = 0,No2 = 0,Div = 0;
+   
+   printf("\n Enter First Number =");
+   scanf("%d",&No1);
+   printf("\n Enter Second Number =");
+   scanf("%d",&No2);
+  
+   Div = No1 / No2;
+   
+   printf("\n Division of Two Numbers = %d",Div);
+   
+   getch();
+   return 0;
+ }

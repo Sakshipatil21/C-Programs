eea1c73ed6b53a9b8f2e99da1d7fa17e120c56f0
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-27 22:04:41 +0530

    Add files via upload

---
+++ Looping Statements/Simple_for.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i = 0;
+  
+  for(i=1;i<=5;i++)
+  {
+     printf("\n\n INDIA");
+     printf("\n I Love My INDIA");
+  }
+  
+  printf("\n\n Thanks !!!");
+  
+  getch();
+  return 0;
+}

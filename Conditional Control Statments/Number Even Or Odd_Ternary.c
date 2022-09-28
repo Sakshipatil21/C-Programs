35f6f03ea5a4fe1100809aa66901d3b2eb7d73cd
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-28 18:30:47 +0530

    Add files via upload

---
+++ Conditional Control Statments/Number Even Or Odd_Ternary.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int Num = 0;
+  
+  printf("\n Enter an Integer Number to Check Whether it is Even or Odd = ");
+  up:
+  scanf("%d",&Num);
+  
+  if(Num == 0)
+  {
+     printf("\n Given Number is Neutral.\n\n Please Enter Other than Zaro Integer to Check Whether it is Even Or Odd = ");
+     goto up;
+  }
+  
+  (Num % 2 == 0) ? printf("\n Given Number is Even.") : printf("\n Given Number is Odd.");
+  
+  printf("\n\n Thanks !!!");
+  
+  getch();
+  return 0;
+}

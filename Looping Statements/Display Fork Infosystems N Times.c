715a9a5f1d2e1db478d9f13aa27f4eed91c3b5b0
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-09-30 21:32:05 +0530

    Add files via upload

---
+++ Looping Statements/Display Fork Infosystems N Times.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int Num = 0,Cnt = 0;
+  
+  printf("\n Enter Any Number For Printing Name Of Class = ");
+  scanf("%d",&Num);
+  
+  printf("\n•••••••••••••• ***** •••••••••••••• \n");
+  
+  for(Cnt = 1;Cnt <= Num;Cnt++)
+  {
+      printf("\n\t FORK INFOSYSTEMS ");
+  }
+  
+  printf("\n\n •••••••••••••• ***** ••••••••••••••");
+  
+  printf("\n\n Thanks !!!");
+  
+  getch();
+  return 0;
+}

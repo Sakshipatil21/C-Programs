3f8fd00a1d42fb7ab3ae4aafebe935b901af4456
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-01 20:44:29 +0530

    Add files via upload

---
+++ Looping Statements/Simple do_while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i = 1;
+  
+  do
+  {
+     printf("\n\n India");
+     printf("\n I Love My India");
+     i++;
+  }while(i<=5);
+  
+  printf("\n\n Thanks !!!");
+  
+  getch();
+  return 0;
+}

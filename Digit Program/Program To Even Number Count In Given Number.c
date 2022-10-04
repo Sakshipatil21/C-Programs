d9b4881ca46edc51b817f6e57deaaf926eeaa41e
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-04 21:39:43 +0530

    Add files via upload

---
+++ Digit Program/Program To Even Number Count In Given Number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int No = 0,Dig = 0,Cnt = 0,Temp = 0;
+   
+   Up:
+     printf("\n\n Enter Positive Integer Number To Count Of Even Number = ");
+     scanf("%d",&No);
+     
+     if(No <= 0)
+     {
+        printf("\n Invalid Number.");
+        getch();
+        goto Up;
+     }
+     
+     Temp = No;
+     
+     while(Temp > 0)
+     {
+        Dig = Temp % 10;
+        
+        if(Dig % 2 == 0 && Dig > 0)
+        {
+           Cnt ++;
+        }
+        Temp = Temp / 10;
+     }
+     printf("\n Count Of Even Number in %d is = %d",No,Cnt);
+     
+     getch();
+     return 0;
+}

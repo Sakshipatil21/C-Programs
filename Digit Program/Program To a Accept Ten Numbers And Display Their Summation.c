64f3cfa04a82bc4f419c528e9cfc154786f87da1
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-08 10:34:11 +0530

    Add files via upload

---
+++ Digit Program/Program To a Accept Ten Numbers And Display Their Summation.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int No=0,Sum=0,Cnt=0;
+  
+  printf("\n Enter Ten Numbes=>");
+  
+  for(Cnt=1;Cnt<=10;Cnt++)
+  {
+    printf("\n\n Enter Number %d = ",Cnt);
+    scanf("%d",&No);
+    
+    Sum = Sum + No;
+  }
+  printf("\n Press any key to get summation of given numbers.");
+  
+  printf("\n\n Summation of given number = %d",Sum);
+  
+  getch();
+  return 0;
+}

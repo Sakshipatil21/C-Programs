e61191b09279e7592c4f729fcc102cb399a79e11
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-09 12:47:41 +0530

    Add files via upload

---
+++ Pattern Printing/Print T Pattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int c = 0,r = 0,rcnt = 0;
+   
+   printf("\n Enter a Number = ");
+   scanf("%d",&rcnt);
+   
+   printf("\n");
+   
+   for(r = 1;r <= rcnt; r ++)
+   {
+      for(c = 1;c <= rcnt;c ++)
+      {
+         if(r == 1 || c == rcnt / 2 + 1)
+         {
+            printf(" * ");
+         }
+         else
+         {
+            printf("   ");
+         }
+      }
+      printf("\n");
+   }
+   getch();
+   return 0;
+}

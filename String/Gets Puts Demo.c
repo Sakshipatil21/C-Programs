669afad64e016e0047ee3035a4916ecb86cc5ea7
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-09 19:34:42 +0530

    Add files via upload

---
+++ String/Gets Puts Demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   char Arr[50] = { };
+   
+   puts("\n Enter a String :");
+   gets(Arr);
+   
+   puts("\n Given String is => ");
+   puts(Arr);
+   
+   getch();
+   return 0;
+}

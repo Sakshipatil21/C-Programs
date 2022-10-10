12d6974498e79c57976d27f1e665d1822d569045
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-10 10:52:01 +0530

    Add files via upload

---
+++ String/String Length_Using while.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   char cSrc[50] = { };
+   int Len = 0;
+   
+   puts("\n Enter a String : ");
+   gets(cSrc);
+   
+   while(cSrc[Len] != '\0')
+   {
+      Len ++;
+   }
+   
+   printf("\n Length of Given String is = %d",Len);
+   
+   getch();
+   return 0;
+ }

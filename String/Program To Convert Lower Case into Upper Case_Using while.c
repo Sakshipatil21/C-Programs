ee1f0c9d6a4f4d073593d5bd5ae2d8b0d7bb0095
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-12 19:32:49 +0530

    Add files via upload

---
+++ String/Program To Convert Lower Case into Upper Case_Using while.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   char cSrc[50] = {'\0'};
+   int i = 0;
+   
+   puts("\n Enter a String = ");
+   gets(cSrc);
+   
+   while(cSrc[i] != '\0')
+   {
+      if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+      {
+         cSrc[i] = cSrc[i] - 32;
+      }
+      i++;
+   }
+   
+   printf("\n Upper Case String is => %s",cSrc);
+   
+   getch();
+   return 0;
+}

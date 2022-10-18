701d791e781a11b4bb9208300ee36608e0798ea2
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-18 15:33:32 +0530

    Add files via upload

---
+++ Array/05.Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Grade[5] ={'A','B','C','D','E'};
+
+    printf("\n\n The Grade Of 1st Student = %c",Grade[0]);
+    printf("\n\n The Grade Of 2nd Student = %c",Grade[1]);
+    printf("\n\n The Grade Of 3rd Student = %c",Grade[2]);
+    printf("\n\n The Grade Of 4th Student = %c",Grade[3]);
+    printf("\n\n The Grade Of 5th Student = %c",Grade[4]);
+
+    printf("\n\n Thanks... \n");
+
+    getch();
+    return 0;
+}
+

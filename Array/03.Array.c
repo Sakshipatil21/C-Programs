45b5f4fb989f4054eb3f0d7a5acdeb1d03f799ba
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-18 15:30:55 +0530

    Add files via upload

---
+++ Array/03.Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5] = {'\0'};
+
+    printf("\n\n The Value Of 1st Element = %d",Num[0]);
+    printf("\n\n The Value Of 2nd Element = %d",Num[1]);
+    printf("\n\n The Value Of 3rd Element = %d",Num[2]);
+    printf("\n\n The Value Of 4th Element = %d",Num[3]);
+    printf("\n\n The Value Of 5th Element = %d",Num[4]);
+
+    printf("\n\n Thanks... \n");
+
+    getch();
+    return 0;
+}
+

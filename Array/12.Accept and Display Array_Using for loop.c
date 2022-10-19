4d53cf7e8fa35b034e2c416f86c208a5c163e1ed
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-19 17:45:12 +0530

    Add files via upload

---
+++ Array/12.Accept and Display Array_Using for loop.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+        int Marks[10] = {'\0'}, i = 0;
+
+        for(i = 0; i < 5; i++)
+        {
+                printf("\n\n Enter Marks Of %d Student = ",i+1);
+                scanf("%d",&Marks[i]);
+        }
+
+        getch();
+        system("cls");
+
+        printf("\n\n *************************************");
+
+        for(i = 0; i < 5; i++)
+        {
+                printf("\n\n Marks Of %d Student = %d",i+1,Marks[i]);
+        }
+        printf("\n\n *************************************");
+
+        printf("\n\n Thanks !!! \n");
+
+        getch();
+        return 0;
+}

4568aa3fe07c8a63fe9f7bb285d6bbf9d219982a
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-10-26 22:05:14 +0530

    Conditional control statement

---
+++ Conditional Control Statments/06_Mall_Nested_if_else.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  float Amount = 0.0;
+  
+  printf("\n Enter Your Balance =");
+  scanf("%f",&Amount);
+  
+  if(Amount > 1000)
+  {
+     printf("\n\n WELCOME to Mall !!!");
+    
+     if(Amount > 10000)
+     {
+        printf("\n\n You Can Be Prime Member Of This Mall !!!");
+     }
+     printf("\n\n Have a Nice Shipping !!!");
+  }
+  else
+  {
+     printf("\n\n Minimum Balance Required To Shop Here Is 1000 + \n");
+    
+     if(Amount > 500)
+     {
+       printf("\t But You Can Visit Our Outlet At Corner !!! \n");
+     }
+     else
+     {
+       printf("\n\t You Are Not Eligible To Shop !!!");
+     }
+     printf("\n\n Thanks For Visit !!!");
+    
+     getch();
+     return 0;
+  }
+}

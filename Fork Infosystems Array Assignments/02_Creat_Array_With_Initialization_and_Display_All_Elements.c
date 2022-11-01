ab675b97549d7db29549ce11b0b4db7848c27248
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-01 20:41:24 +0530

    Add files via upload

---
+++ Fork Infosystems Array Assignments/02_Creat_Array_With_Initialization_and_Display_All_Elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int Arr[5] = {70,33,12,90,58};
+   
+   printf("\n\n The 1st Element is = %d",Arr[0]);
+   printf("\n\n The 2nd Element is = %d",Arr[1]);
+   printf("\n\n The 3rd Element is = %d",Arr[2]);
+   printf("\n\n The 4th Element is = %d",Arr[3]);
+   printf("\n\n The 5th Element is = %d",Arr[4]);
+   
+   printf("\n\n Thanks !!!");
+   
+   getch();
+   return 0;
+}

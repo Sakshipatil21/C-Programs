8f6c2a525b82b5499c66b26e0f4f0558443a6d78
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-01 16:47:22 +0530

    Array Program

--- Array/03_Array.c
+++ Array/03_Array.c
@@ -11,7 +11,7 @@ int main()
     printf("\n\n The Value Of 4th Element = %d",Num[3]);
     printf("\n\n The Value Of 5th Element = %d",Num[4]);
 
-    printf("\n\n Thanks... \n");
+    printf("\n\n Thanks.... \n");
 
     getch();
     return 0;

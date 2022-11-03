f83f2532331125c7b608bd8363c31b8fc4b8f25e
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-03 18:17:50 +0530

    Basic Program

---
+++ Basic Programs/09_Size_of_Data_Types.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n Size of int Data Type = %d",sizeof(int));
+    printf("\n Size of char Data Type = %d",sizeof(char));
+    printf("\n Size of float Data Type = %d",sizeof(float));
+    printf("\n Size of void Data Type = %d",sizeof(void));
+    
+    printf("\n\n Thanks !!!");
+    
+    getch();
+    return 0;
+}

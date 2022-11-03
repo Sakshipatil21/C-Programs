8a96fb732b160e5ba7185129bc9a98a794f95376
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-03 17:55:48 +0530

    Basic Program

--- Basic Programs/07_Students_Information_Database.c
+++ Basic Programs/07_Students_Information_Database.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <conio.h>
+#include<stdio.h>
+#include<conio.h>
 
 int main()
 {
     printf("\n ===============*******=============== \n");
     
-    printf("\n \t Student Information Database\n");
+    printf("\n\t Student Information Database\n");
     
-    printf("\n %5d %-10s %3d %3d %3d %3d %0.2f %-16s",120,"Amitabh",6,95,65,166,55.33333,"Pune");
+    printf("\n\n %5d %-10s %3d %3d %3d %3d %0.2f %-16s",120,"Amitabh",6,95,65,166,55.33333,"Pune");
     printf("\n\n %5d %-10s %3d %3d %3d %3d %0.2f %-16s",6,"Raj",55,80,180,60,84.7245,"Kolhapur");
     printf("\n\n %5d %-10s %3d %3d %3d %3d %0.2f %-16s",4138,"Virat",65,85,50,200,66.78468,"Ratnagiri");
     
-    printf("\n ===============*******=============== \n");
+    printf("\n\n ===============*******=============== \n");
     
     getch();
     return 0;

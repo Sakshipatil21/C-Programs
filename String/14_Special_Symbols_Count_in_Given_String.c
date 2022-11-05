324d30df5747eb0cacf0b2dfef99e1618b75f8a0
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-05 19:47:30 +0530

    String Program

---
+++ String/14_Special_Symbols_Count_in_Given_String.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+        int Cnt = 0, i = 0;
+
+        printf("\n Enter a String = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+                if(!((cSrc[i] >= 'A'&& cSrc[i] <= 'Z') || (cSrc[i] >= 'a'&& cSrc[i] <= 'z') || (cSrc[i] >= '0'&& cSrc[i] <= '9')))
+                {
+                    Cnt++;
+                }
+                i++;
+        }
+
+        printf("\n Special Symbols Count in Given String = %d",Cnt);
+
+        printf("\n\n Thanks !!!");
+
+        getch();
+        return 0;
+}

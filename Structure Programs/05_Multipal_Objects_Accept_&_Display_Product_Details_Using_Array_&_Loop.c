b9474f6ced2f53630b7b71343ce75744cca03f7e
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-10 18:11:12 +0530

    Structure Program

---
+++ Structure Programs/05_Multipal_Objects_Accept_&_Display_Product_Details_Using_Array_&_Loop.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Product
+{
+       int P_ID;
+       char P_Name[20];
+       float P_P_Price;
+       float P_S_Price;
+};
+
+int main()
+{
+       int i = 0;
+       struct Product Prod[3];
+       
+       printf("\n\n Accepting Product Details => \n");
+       
+       for(i = 0; i < 3; i++)
+       {
+            printf("\n\n Enter Product ID = ");
+            scanf("%d",&Prod[i].P_ID);
+            
+            printf("\n Enter Product Name = ");
+            scanf(" %[^\n]",&Prod[i].P_Name);
+            
+            printf("\n Enter Product Purchase Price = ");
+            scanf(" %f",&Prod[i].P_P_Price);
+            
+            printf("\n Enter Product Sales Price = ");
+            scanf("%f",&Prod[i].P_S_Price);
+       }
+       
+       getch();
+       system("cls");
+       
+       printf("\n ================================= \n");
+       
+       printf("\n Display Product Details =>");
+       
+       for(i = 0; i < 3; i++)
+       {
+            printf("\n\n %d Product Details => \n",i+1);
+            
+            printf("\n\t ID = %d.",Prod[i].P_ID);
+            printf("\n\t Name = %s.",Prod[i].P_Name);
+            printf("\n\t Purchase Price = %0.2f",Prod[i].P_P_Price);
+            printf("\n\t Sales Price = %0.2f",Prod[i].P_S_Price);
+       }
+       
+       printf("\n\n =============================== \n");
+       
+       printf("\n Thanks !!!");
+       
+       getch();
+       return 0;
+}

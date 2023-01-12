db97326f9ed03032303961226bb370971a618c61
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2023-01-12 20:41:31 +0530

    Add files via upload

--- Fork Infosystems Pattern Printing Assignments/01_Pattern_Printing_Assignment.c
+++ Fork Infosystems Pattern Printing Assignments/01_Pattern_Printing_Assignment.c
@@ -9,24 +9,32 @@
 
 int main()
 {
-         int r = 0, c = 0, cnt = 0;
-         char ch = 'A';
+      int r = 0, c = 0, cnt = 0;
+      char ch = 'A';
 
-         printf("\n Enter Count of Rows and Columns = ");
-         scanf("%d",&cnt);
+      printf("\n Enter Count of Rows and Columns = ");
+      scanf("%d",&cnt);
 
-         printf("\n Pattern is => \n\n");
+      printf("\n Pattern is => \n\n");
 
-         for(r = 1; r <= cnt; r++)
-         {
-                  for(c = 1; c <= cnt; c++)
-                  {
-                           if(r >= c)
-                           {
-                                 printf("%2c",ch);
-                                 ch++;
-                           }
-                  }
+      for(r = 1; r <= cnt; r++)
+      {
+          for(c = 1; c <= cnt; c++)
+          {
+               if(r >= c)
+               {
+                  printf("%2c",ch);
+                  ch++;
+               }
+          }
+          printf("\n");
+      }
+
+      printf("\n Thanks !!!");
+
+      getch();
+      return 0;
+}            }
                   printf("\n");
          }
 

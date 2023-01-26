ab84c2c7d4d42110553ff0794a16ff0b45c7e894
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2023-01-26 19:26:42 +0530

    Add files via upload

---
+++ Fork Infosystems Pattern Printing Assignments/10_Pattern Printing Assignment.c
@@ -0,0 +1,35 @@
+//Q
+//Q  Q
+//Q  Q  Q
+//Q  Q  Q
+//Q  Q  Q  Q
+
+#include<stdio.h>
+#include<conio.h> 
+
+int main() 
+{
+     int r = 0,c = 0,cnt = 0;
+   
+     printf("\n Enter Count of Rows and Columns = ");
+     scanf("%d",&cnt);
+    
+     printf("\n Pattern is => \n\n");
+   
+     for(r = 1;r <= cnt;r++) 
+     {
+         for(c = 1;c <= cnt;c++) 
+         {
+             if(r >= c)
+             {
+                printf(" Q ");
+             }
+         }
+         printf("\n");
+     }
+    
+     printf("\n Thanks !!!");
+    
+     getch();
+     return 0;
+}
